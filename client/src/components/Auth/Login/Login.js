c941c24f05cb72160e8d8fd6f75c8ffc2b589484
Daniel <dtues003@fiu.edu>
2020-09-26 04:42:59 -0400

    finished authentication

--- client/src/components/Auth/Login/Login.js
+++ client/src/components/Auth/Login/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import LockIcon from "../../../assets/icons/LockIcon";
@@ -7,21 +7,43 @@ import classes from "./Login.module.css";
 import TextInput from "../../UI/Inputs/TextInput/TextInput";
 import Button from "../../UI/Button/Button";
 import Paper from "../../UI/Paper/Paper";
+import axios from "../../../shared/axios-api";
+import { AuthContext } from "../../../shared/AuthContext";
 
 const Login = props => {
+	const { setUser, setIsAuthenticated } = useContext(AuthContext);
 	const { register, errors, handleSubmit, setError } = useForm({
 		reValidateMode: "onSubmit",
 		shouldFocusError: true,
 	});
 
-	const onSubmit = () => {};
+	const onSubmit = data => {
+		axios
+			.post("/auth/login", data)
+			.then(res => {
+				setIsAuthenticated(true);
+				setUser(res.data.data);
+			})
+			.catch(err => {
+				const username = document.getElementById("username");
+				username.focus();
+				setError("username", {
+					type: "manual",
+					message: err.response.data.error,
+				});
+				setError("password", {
+					type: "manual",
+					message: err.response.data.error,
+				});
+			});
+	};
 
 	const blueDark = getComputedStyle(document.documentElement).getPropertyValue("--blue-dark");
 	const usernameError = errors.username ? errors.username.message : null;
 	const passwordError = errors.password ? errors.password.message : null;
 
 	return (
-		<div className='center'>
+		<div className="center">
 			<div className={classes.LoginContainer}>
 				<Paper>
 					<form onSubmit={handleSubmit(onSubmit)} className={classes.LoginForm}>
