3c51a988636ff17fb67380c7c9e0c64f5f6cadf0
Daniel <dtues003@fiu.edu>
2020-09-26 00:46:12 -0400

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import Login from "./components/Auth/Login/Login";
+import Register from "./components/Auth/Register/Register";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 const App = () => {
-  return (
-    <div className="App">
+	const [authenticated, setAuthenticated] = useState(false);
 
-    </div>
+	let routes = (
+		<Switch>
+			<Route path="/login" component={Login} />
+			<Route path="/register" component={Register} />
+			<Redirect path="/register" component={Register} />
+		</Switch>
   );
-}
+  
+  if (authenticated) {
+    routes = (
+      <div>Home</div>
+    )
+  }
+
+	return <div className="App">{routes}</div>;
+};
 
 export default App;
---
+++ client/src/components/UI/Button/Button.js
@@ -0,0 +1,26 @@
+import React from "react";
+import classes from "./Button.module.css";
+
+const Button = props => {
+	const { color, outlined, width, children, ...rest } = props;
+	let style = {
+		backgroundColor: color,
+		border: "1.5px solid " + color,
+		width,
+	};
+	if (outlined) {
+		style = {
+			...style,
+			color,
+			backgroundColor: "transparent",
+		};
+	}
+
+	return (
+		<button className={classes.Button} style={style} {...rest}>
+			{children}
+		</button>
+	);
+};
+
+export default Button;
