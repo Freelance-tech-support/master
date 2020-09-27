f4c35cd9f77941a5097fd23cc43f48d840df1f42
Daniel <dtues003@fiu.edu>
2020-09-27 08:46:36 -0400

    add

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ connectDB();
 
 const app = express();
 const server = http.createServer(app);
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 //Socket.io
 const io = socket(server);
